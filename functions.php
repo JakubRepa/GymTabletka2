9292432e2665f883862bbad98c2646c668ee9c13
Jakub <jakrep01@easv365.dk>
2024-10-10 18:01:12 +0200

    woocommerce enabled

--- functions.php
+++ functions.php
@@ -34,8 +34,7 @@ function disable_gutenberg_for_home_page( $use_block_editor, $post_type ) {
 }
 add_filter( 'use_block_editor_for_post_type', 'disable_gutenberg_for_home_page', 10, 2 );
 
-function add_woocommerce_support()
-{
+function add_woocommerce_support(){
    add_theme_support('woocommerce');
 }
 
