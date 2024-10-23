20484f771c321200855e95fdc464a9e60cca8f3a
patrik-sevcik <patsev01@easv365.dk>
2024-10-23 18:35:58 +0200

    Update page-my-account.php

--- page-my-account.php
+++ page-my-account.php
@@ -3,12 +3,3 @@
     <?php echo do_shortcode('[woocommerce_my_account]'); ?>
 
 <?php get_footer(); ?>
-<style>
-    footer{
-        padding-top:50px;
-    }
-
-    header{
-        padding-bottom:50px;
-    }
-</style>
