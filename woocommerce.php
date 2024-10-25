fb8cceba6cf054ad682d7d329ff1ec5936779b33
patrik-sevcik <patsev01@easv365.dk>
2024-10-25 13:30:07 +0200

    feat: add woo wrapper container

--- woocommerce.php
+++ woocommerce.php
@@ -1,5 +1,5 @@
 <? get_header(); ?>
-    <div class="mt-32 h-[calc(100vh-8rem)]">
+    <div class="mt-32 h-[calc(100vh-8rem)] container mx-auto">
     <? woocommerce_content() ?>
     </div>
 <? get_footer(); ?>
