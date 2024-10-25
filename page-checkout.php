454691b5e789b3f6bb022f0dc37bf06f96d8bfc0
patrik-sevcik <patsev01@easv365.dk>
2024-10-25 13:41:58 +0200

    feat: checkout wrapper

--- page-checkout.php
+++ page-checkout.php
@@ -1,32 +1,31 @@
-<?php get_header(); ?>
-
-    <?php echo do_shortcode('[woocommerce_checkout]'); ?>
-
-<?php get_footer(); ?>
+<? get_header(); ?>
+<div class="mt-32 h-[calc(100vh-8rem)] container mx-auto">
+    <? echo do_shortcode('[woocommerce_checkout]'); ?>
+</div>
+<? get_footer(); ?>
 
 <style>
-    .woocommerce{
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-        justify-content:center;
-        margin-top: 200px; 
-        }
-
-    form{
-        width:500px;
-        margin-top: 200px; 
-        margin-bottom: 110px; 
+    .woocommerce {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin-top: 200px;
     }
 
-    ul{
-        display:flex;
+    form {
+        width: 500px;
+        margin-top: 200px;
+        margin-bottom: 110px;
+    }
+
+    ul {
+        display: flex;
         width: full;
     }
 
-    li{
-        display:flex;
-        width:full;
+    li {
+        display: flex;
+        width: full;
     }
-
 </style>
