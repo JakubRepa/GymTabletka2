99a12a6c3714521a9d12a3b33b30a1232355f42c
patrik-sevcik <patsev01@easv365.dk>
2024-10-25 13:49:56 +0200

    feat: add padding to wrapper

--- page-checkout.php
+++ page-checkout.php
@@ -1,9 +1,5 @@
 <? get_header(); ?>
-<div class="mt-32 h-full min-h-[calc(100vh-8rem)] container mx-auto max-w-6xl">
+<div class="mt-28 h-full min-h-[calc(100vh-8rem)] container mx-auto max-w-6xl py-8">
     <? echo do_shortcode('[woocommerce_checkout]'); ?>
 </div>
 <? get_footer(); ?>
-
-<h1>
-    cs
-</h1>
--- woocommerce.php
+++ woocommerce.php
@@ -1,5 +1,5 @@
 <? get_header(); ?>
-<div class="mt-32 h-full min-h-[calc(100vh-8rem)] container mx-auto max-w-6xl">
+<div class="mt-28 h-full min-h-[calc(100vh-8rem)] container mx-auto max-w-6xl py-8">
     <? woocommerce_content() ?>
 </div>
 <? get_footer(); ?>
