93aae3a7bb68f4c0ba137ccdfe44b3810072c3cc
patrik-sevcik <patsev01@easv365.dk>
2024-10-25 13:46:05 +0200

    fix: checkout and basket height

--- page-basket.php
+++ page-basket.php
@@ -1,5 +1,5 @@
 <? get_header(); ?>
-<div class="mt-32 h-[calc(100vh-8rem)] container mx-auto">
+<div class="mt-32 h-full min-h-[calc(100vh-8rem)] container mx-auto">
     <? echo do_shortcode('[woocommerce_cart]'); ?>
 </div>
 <? get_footer(); ?>
