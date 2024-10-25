d3a58d8a3cd77916fb0ea9fc428238d7eb559ee7
patrik-sevcik <patsev01@easv365.dk>
2024-10-25 13:47:28 +0200

    feat: add max width

--- page-checkout.php
+++ page-checkout.php
@@ -1,5 +1,5 @@
 <? get_header(); ?>
-<div class="mt-32 h-full min-h-[calc(100vh-8rem)] container mx-auto">
+<div class="mt-32 h-full min-h-[calc(100vh-8rem)] container mx-auto max-w-6xl">
     <? echo do_shortcode('[woocommerce_checkout]'); ?>
 </div>
 <? get_footer(); ?>
